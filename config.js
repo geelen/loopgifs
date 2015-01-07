b1d33e04c272302e8cd8eed0914e0a8cc9eec539
Glen Maddern <glenmaddern@gmail.com>
2015-01-07 17:51:23 +1100

    completed demo

---
+++ config.js
@@ -0,0 +1,49 @@
+System.config({
+  "paths": {
+    "*": "*.js",
+    "app/*": "lib/*.js",
+    "github:*": "jspm_packages/github/*.js",
+    "npm:*": "jspm_packages/npm/*.js"
+  }
+});
+
+System.config({
+  "map": {
+    "jsonp": "npm:jsonp@0.1.0",
+    "github:jspm/nodelibs-fs@0.1.0": {
+      "assert": "npm:assert@1.3.0",
+      "fs": "github:jspm/nodelibs-fs@0.1.0"
+    },
+    "github:jspm/nodelibs-process@0.1.0": {
+      "process": "npm:process@0.10.0"
+    },
+    "github:jspm/nodelibs-tty@0.1.0": {
+      "tty-browserify": "npm:tty-browserify@0.0.0"
+    },
+    "github:jspm/nodelibs-util@0.1.0": {
+      "util": "npm:util@0.10.3"
+    },
+    "npm:assert@1.3.0": {
+      "util": "npm:util@0.10.3"
+    },
+    "npm:debug@2.1.1": {
+      "fs": "github:jspm/nodelibs-fs@0.1.0",
+      "ms": "npm:ms@0.6.2",
+      "net": "github:jspm/nodelibs-net@0.1.0",
+      "process": "github:jspm/nodelibs-process@0.1.0",
+      "tty": "github:jspm/nodelibs-tty@0.1.0",
+      "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "npm:inherits@2.0.1": {
+      "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "npm:jsonp@0.1.0": {
+      "debug": "npm:debug@2.1.1"
+    },
+    "npm:util@0.10.3": {
+      "inherits": "npm:inherits@2.0.1",
+      "process": "github:jspm/nodelibs-process@0.1.0"
+    }
+  }
+});
+
